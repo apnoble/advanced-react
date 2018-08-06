76f5c47d32dbf8fc406d6c3eaeca4e442dea2e95
Alex Noble <AWGR-Alex@comcast.net>
2018-08-06 15:23:09 -0700

    make it show Hello React

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,37 @@
 module.exports = {
-    parser: 'babel-eslint',
-    env: {
-      browser: true,
-      commonjs: true,
-      es6: true,
-      node: true,
-      jest: true,
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
     },
-    extends: ['eslint:recommended', 'plugin:react/recommended'],
-    parserOptions: {
-      ecmaFeatures: {
-        experimentalObjectRestSpread: true,
-        jsx: true,
-      },
-      sourceType: 'module',
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
     },
-    plugins: ['react'],
-    rules: {
-      'react/prop-types': ['off'],
-      indent: ['error', 2, { SwitchCase: 1 }],
-      'linebreak-style': ['error', 'unix'],
-      quotes: ['error', 'single'],
-      semi: ['error', 'always'],
-      'no-console': ['warn', { allow: ['info', 'error'] }],
-      'arrow-parens': ['error', 'always'],
-    },
-  };
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "no-unused-vars":0,
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
